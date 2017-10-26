749a7b667f201152b5f48038a6b0bac313177f08
Nishan <nishan@vivek.tech>
2017-10-26 12:37:47 -0700

    Initial Commits

--- client.py
+++ client.py
@@ -25,8 +25,9 @@ def main():
 #Main command and display loop
     while 1:
             command = raw_input("Command to send:") + "#" + args.source_ip
-            print (command)
+            #print (command)
             packet = IP(dst=args.dest, src=args.source_ip)/UDP(sport=int(args.source_port), dport=int(args.port))/command
+            print packet['Raw'].load
             send(packet)
             sniff(filter="udp and src port " + args.port + " and dst port 8081", stop_filter=stp_filter)
 
